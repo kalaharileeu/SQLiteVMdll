c1e796dcfb31e49bdb728f2987591612b3f2b77b
cornel de lange <cfdelange@gmail.com>
2017-05-09 06:57:02 +1200

    added some comments

--- SQLitedllVM/EF/UserContext.cs
+++ SQLitedllVM/EF/UserContext.cs
@@ -2,8 +2,10 @@
 
 namespace SQLitedllVM.Models
 {
+	///The context instance gets past around, for the repo pattern to get entry to the data
     public class UserContext : DbContext
     {
+		
         public DbSet<Userdetail> Users { get; set; }
         public DbSet<Point> UserPoints { get; set; }
 
